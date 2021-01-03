a343dd487f61332b4d819898d36d760e50ac6c65
wat1881mayer <happymayer2013@icloud.com>
2021-01-03 16:15:52 +0900

    apply grid

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles'
+import MessageInputField from './MessageInputField'
+import MesseageList from './MesseageList'
 
-const Main = ()=> {
-    return <div>Main</div>;
+const useStyles =  makeStyles({
+    root:{
+        display:'grid',
+        height:'100vh',
+        gridTemplateRows:'1fr auto'
+    }
+})
+const Main = ({name})=> {
+    const classes = useStyles();
+    return (<div className={classes.root}>
+        <MesseageList />
+        <MessageInputField name={name}/>
+        </div>
+        )
 };
 
 export default  Main;
